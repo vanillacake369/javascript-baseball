ef3e85482ff9c46efe46032a541d0abc43d9d1ee
Laptop-Limjihoon <lonelynight1026@gmail.com>
2022-11-04 20:28:25 +0900

    user input 가져오는 기능 구현 - readline 객체 : get keyboard input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+// readline 객체 : get keyboard input
+const readline = require("readline");
+const rl = readline.createInterface({
+  input:process.stdin,
+  output:process.stdout,
+  terminal: false
+});
+rl.on('line',(line)=>{
+  console.log(line);
+});
+rl.once('close',()=>{
+  // end of input
+});
+
 class App {
-  play() {}
+  play() {
+    console.log("숫자 야구 게임을 시작합니다.");
+  }
 }
+
+while(computer.length < 3){
+
+}
+
 
 module.exports = App;
