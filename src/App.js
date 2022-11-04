b67eaca8a08b95c9532aa05f3e81f83908d1c78f
Laptop-Limjihoon <lonelynight1026@gmail.com>
2022-11-04 22:47:56 +0900

    사용자 입력값 배열화 기능 구현 - MissionUtils.Console.readLine - 입력문자열의 크기만큼 각 인덱스에 접근 이후, Number와 push를 통해 사용자 입력값을 배열화함

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const computer = [];
-const player = [];
+const Console = require("console");
+let computer = [];
+let player = [];
 // console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
 
 class App {
   play() {
     generateAnswer();
-    computer.forEach(element=>{
-      MissionUtils.Console.print(element + '<br>');
-    })
-    // MissionUtils.Console.print('안녕하세요.');
-    // MissionUtils.Console.print(MissionUtils.Random.pickNumberInList([1, 9]));
+    getUserInput();
+    /*let str_arr = [];
+    let str = "215";
+    str_arr.push(Number(str.charAt(0)));
+    str_arr.push(Number(str.charAt(1)));
+    str_arr.push(Number(str.charAt(2)));
+
+    MissionUtils.Console.print(str_arr);
+    MissionUtils.Console.print(str.length);*/
+    MissionUtils.Console.print(player);
+    MissionUtils.Console.print(computer);
+    // player.forEach(element => {
+    //   MissionUtils.Console.print(element);
+    // })
   }
+}
+
+const getUserInput = () => {
+  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (userInput) => {
+    for (let i=0;i<userInput.length;i++){
+      player.push(Number(userInput.charAt(i)));
+    }
+  });
+}
+
+const isValidInput = () => {
+  return true;
+}
+
+const isStrikeOut = () => {
+  return true;
 }
 
 const generateAnswer = () => {
