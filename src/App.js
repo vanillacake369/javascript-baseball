deb64750a1e77ba3c2b8aa00f02ccedfc7b54517
NGN-Limjihoon <lonelynight1026@gmail.com>
2022-11-05 18:02:54 +0900

    사용자 입력에 따라 게임 재시작여부 함수 구현 - restartBaseballGame() - 입력이 1이면 play() - 입력이 2이면 process 종료

--- src/App.js
+++ src/App.js
@@ -6,29 +6,31 @@ let player = [];
 
 class App {
   play() {
-    generateAnswer();
-    getUserInput();
-    /*let str_arr = [];
-    let str = "215";
-    str_arr.push(Number(str.charAt(0)));
-    str_arr.push(Number(str.charAt(1)));
-    str_arr.push(Number(str.charAt(2)));
-
-    MissionUtils.Console.print(str_arr);
-    MissionUtils.Console.print(str.length);*/
-    MissionUtils.Console.print(player);
+    getBaseballGameSetting();
     MissionUtils.Console.print(computer);
-    // player.forEach(element => {
-    //   MissionUtils.Console.print(element);
-    // })
+    MissionUtils.Console.print(player);
+
   }
 }
 
+const getBaseballGameSetting = () => {
+  generateAnswer();
+  getUserInput();
+}
+
+const restartBaseballGame = () => {
+  MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (gameInitButton) => {
+    if(gameInitButton.equal(1)){
+      play();
+    }
+  });
+}
+
 const getUserInput = () => {
-  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (userInput) => {
-    for (let i=0;i<userInput.length;i++){
-      player.push(Number(userInput.charAt(i)));
+  MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (userGuessingNumber) => {
+    for (let i=0;i<userGuessingNumber.length;i++){
+      player.push(Number(userGuessingNumber.charAt(i)));
     }
   });
 }
